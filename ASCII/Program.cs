94f2df2c3e0ead15ada520121511ff8d2211361f
FANUX04 <gabry04.gf@gmail.com>
2020-11-26 11:45:05 +0100

    codice UNICODE

--- ASCII/Program.cs
+++ ASCII/Program.cs
@@ -9,7 +9,9 @@ namespace ASCII
             //Console.WriteLine("inserisci una frase");
             //string unicodestring = Console.ReadLine();
 
+
             string unicodestring =
+
                 "this string has 2 characters outside the" +
                 "ASCII range: \n" +
                 "radiazione (\u2622) , and sigma (\u03A0).";
