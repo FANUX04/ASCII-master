66f9c9b7e2b21f1bb829adf6d408e11230324e7c
FANUX04 <gabry04.gf@gmail.com>
2020-11-26 11:44:29 +0100

    Aggiungere i file di progetto.

---
+++ ASCII/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("inserisci una frase");
+            //string unicodestring = Console.ReadLine();
+
+            string unicodestring =
+                "this string has 2 characters outside the" +
+                "ASCII range: \n" +
+                "radiazione (\u2622) , and sigma (\u03A0).";
+            Console.WriteLine("stringa originale/original string");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(unicodestring);
+            Console.WriteLine();
+        }
+    }
+}
